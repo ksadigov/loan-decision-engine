c27dd30f2817fe7591442ec9734848379be90a97
ksadigov <kamransadigov@gmail.com>
2024-03-04 13:39:07 +0200

    Fixed controller

--- src/main/java/com/inbank/decision/engine/controller/LoanDecisionController.java
+++ src/main/java/com/inbank/decision/engine/controller/LoanDecisionController.java
@@ -4,6 +4,7 @@ import com.inbank.decision.engine.dto.LoanApplicationDto;
 import com.inbank.decision.engine.dto.LoanApplicationResultDto;
 import com.inbank.decision.engine.service.LoanDecisionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/loan")
+@RequestMapping("/api/v1/loans")
 @RequiredArgsConstructor
 public class LoanDecisionController {
 
@@ -19,6 +20,6 @@ public class LoanDecisionController {
 
     @PostMapping("/decision")
     public ResponseEntity<LoanApplicationResultDto> makeLoanDecision(@RequestBody LoanApplicationDto application) {
-        return ResponseEntity.ok(loanDecisionService.makeDecision(application));
+        return new ResponseEntity<>(loanDecisionService.makeDecision(application), HttpStatus.OK);
     }
 }
